b314a7a2ba57d2cc5487ccede90193cc627bee11
Michail Marakis <p3230267@aueb.gr>
2026-04-12 17:30:00 +0300

    finalChangeReport

---
+++ integration_with_jumps.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+#include <pthread.h>
+
+//#define N 100000000
+#define N 1000
+#define NUM_THREADS 8
+
+//integration limits
+double a = -50.0, b = 50.0;
+
+//step size
+double h;
+
+//global sum (shared between threads)
+double sum = 0.0;
+
+//mutex to protect shared variable
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+//functions to integrate
+double f(double x) {
+    // return x * x;
+    // return exp(-(x*x));
+    return cos(x * x);
+}
+
+//thread function
+void* compute(void* arg) {
+    int id = *(int*)arg;
+
+    double local_sum = 0.0;
+
+    //cyclic distribution of work
+    //each thread takes elements: i = id, id + T, id + 2T, etc...
+    for (int i = id; i < N; i = i + NUM_THREADS) {
+        double x = a + i * h;
+        local_sum += 2 * f(x);
+    }
+
+    //update global sum safely
+    //locking the mutex
+    pthread_mutex_lock(&mutex);
+    sum += local_sum;
+
+    //unlocking the mutex for the next thread to update on the shared variable
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
+}
+
+int main() {
+
+    h = (b - a) / N;
+
+    pthread_t threads[NUM_THREADS];
+    int ids[NUM_THREADS];
+
+    pthread_mutex_init(&mutex, NULL);
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    //create the threads
+    for (int i = 0; i < NUM_THREADS; i++) {
+        ids[i] = i;
+        pthread_create(&threads[i], NULL, compute, &ids[i]);
+    }
+
+    //wait for all of the threads to finish
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    //add boundary points for the trapezoidal rule
+    sum += f(a) + f(b);
+
+    double final_res = sum * (h / 2);
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end_time - start_time;
+
+    std::cout << "Integral = " << final_res << std::endl;
+    std::cout << "Time = " << elapsed.count() << " sec\n";
+
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}

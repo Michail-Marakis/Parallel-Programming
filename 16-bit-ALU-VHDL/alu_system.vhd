8af23e4f7605d3ea8a6f0f13dcd6c7d042ae99c9
mike <mikemarakmike@gmail.com>
2026-06-30 19:57:06 +0300

    Update alu_system.vhd

--- 16-bit-ALU-VHDL/alu_system.vhd
+++ 16-bit-ALU-VHDL/alu_system.vhd
@@ -11,7 +11,8 @@ ENTITY ALU16bit IS
 END ALU16bit;
 
 ARCHITECTURE ALU16bit_arch OF ALU16bit IS
-    COMPONENT Part1 IS   													--dimiourgoume component apo to part1, alu-1bit gia na thn xrisimopoihsoyme me tis prajeis mas bit me bit
+    COMPONENT Part1 IS   									-- We create a component from Part1 (1-bit ALU)
+        													-- in order to use it for bit-by-bit operations
         PORT ( A, B, AINVERT, BINVERT, Cin : IN STD_LOGIC;
                OPERATION : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
                Cout, F : OUT STD_LOGIC
@@ -19,14 +20,14 @@ ARCHITECTURE ALU16bit_arch OF ALU16bit IS
     END COMPONENT;
 
     SIGNAL C : STD_LOGIC_VECTOR(15 DOWNTO 0);
-																--ta simata apo slice se slice
+																--carry signals from slice to slice
 	 SIGNAL S : STD_LOGIC_VECTOR(15 DOWNTO 0);
 
 BEGIN
-    -- ylopoihsh prwtou slice
+    --implementation of first slice
     first_slice: Part1 PORT MAP (A(0), B(0), Ainvert, Binvert, CIN, OPERATION, C(0), S(0));
 
-    -- ftiaxnoume kai ta ypoloipa slice
+    --creation of the remaining slices
     other_slices: FOR i IN 1 TO 15 GENERATE
     slices_ALU : Part1 PORT MAP (A(i), B(i), Ainvert, Binvert, C(i-1), OPERATION, C(i), S(i));
     
@@ -45,15 +46,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 ENTITY prajeis IS
-    PORT ( opcode : IN STD_LOGIC_VECTOR(2 DOWNTO 0);	--to opcode exei mikos 3
+    PORT ( opcode : IN STD_LOGIC_VECTOR(2 DOWNTO 0);	--opcode has length 3 bits
            Ainvert, Binvert, CARRYIN : OUT STD_LOGIC;
-           OPERATION : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)  --to operation exei mikos 2
+           OPERATION : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)  --operation has length 2 bits
 			  ); 
 END prajeis;
 
 ARCHITECTURE prajeis_arch OF prajeis IS
 BEGIN
-    PROCESS(opcode)			--ylopoihsh twn prajewn
+    PROCESS(opcode)			--implementation of operations
     BEGIN
         CASE opcode IS
             WHEN "000" => -- and
@@ -91,7 +92,7 @@ BEGIN
                 Binvert <= '0';
                 CARRYIN <= '0';
                 OPERATION <= "11";
-            WHEN OTHERS => --den xrisimopoihte to opcode = 111
+            WHEN OTHERS => --opcode = 111 not used
                 Ainvert <= '0';
                 Binvert <= '0';
                 CARRYIN <= '0';
@@ -107,7 +108,7 @@ use ieee.std_logic_1164.all;
 
 ENTITY Part_2 IS
     PORT ( A, B : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-           opcode : IN STD_LOGIC_VECTOR(2 DOWNTO 0); 			--orismos koryfaias entity
+           opcode : IN STD_LOGIC_VECTOR(2 DOWNTO 0); 			
            result : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
            COUT, overflow : OUT STD_LOGIC
     );
@@ -124,7 +125,7 @@ ARCHITECTURE Part_2_arch OF Part_2 IS
                result : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
                COUT, overflow : OUT STD_LOGIC
         );
-    END COMPONENT;															--orizoune ws components ta entities poy ftiajame
+    END COMPONENT;															
 
     COMPONENT prajeis IS
         PORT ( opcode : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
@@ -142,6 +143,6 @@ BEGIN
 																																						--
 	 stage2: ALU16bit PORT MAP (A, B, Ainvert, Binvert, CARRYIN, OPERATION, result, internal_COUT, overflow);
 
-    COUT <= internal_COUT; 														--apo edw tha prokypsei teleftaia timh kratoumenoy gia elegxo overflow
+    COUT <= internal_COUT; 								 --from here we get the final carry-out value for overflow checking
 
 END Part_2_arch;
